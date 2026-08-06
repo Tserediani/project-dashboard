4eae5293983592694313d18887847463b1b2b74e
Giorgi Tserediani <tserediani.g@gmail.com>
2026-08-06 17:18:43 +0400

    chore(config): add application settings with postgres configuration

---
+++ src/project_dashboard/config.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Secret
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class PostgresConfig(BaseModel):
+    protocol: str
+    host: str
+    port: str
+    user: Secret[str]
+    password: Secret[str]
+    db: Secret[str]
+
+    @property
+    def dsn(self) -> str:
+        return (
+            f"{self.protocol}://{self.user.get_secret_value()}:"
+            f"{self.password.get_secret_value()}@{self.host}:"
+            f"{self.port}/{self.db.get_secret_value()}"
+        )
+
+
+class AppConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        extra="ignore",
+    )
+
+    postgres: PostgresConfig
+
+
+CONFIG = AppConfig()
